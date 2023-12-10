66954a860bf1ee29f06feead4bda5482713d9974
Javier Pacheco <jpacheco@cock.li>
2023-12-10 10:51:48 -0600

    clean noobemacs

--- init.el
+++ init.el
@@ -7,3 +7,16 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-bullets visual-fill-column company-box company ef-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number ((t (:background nil)))))
