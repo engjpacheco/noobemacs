197eaf6d1d5c65b8c04668296bef29b6f1d00b56
Javier Pacheco <jpacheco@cock.li>
2024-02-17 07:32:36 -0600

    update early-init.org

--- early-init.el
+++ early-init.el
@@ -1,1 +1,12 @@
-(setq package-enable-at-startup nil)
+;; Load this dark theme, to keep your eyes alive.
+(load-theme 'tango-dark)
+
+;; Disable GUI when foundit
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
